bb71f4755d44a4f4d15a8fad2bcbd1c3453bb4b2
SamAkim <63521202+SamAkim@users.noreply.github.com>
2022-07-29 15:25:55 +0530

    Add files via upload

---
+++ Text2Speech.py
@@ -0,0 +1,18 @@
+#pyttsx3 package is mustbe installed first in order for the programm to work
+import pyttsx3  #this is the package which works on offline if you need online you can choose gts package
+
+engine = pyttsx3.init()
+for voice in engine.getProperty("voices"): #getting voices which are available in our system
+    print(voice)
+
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id) # choosing voice
+
+def Speak(Audio): #function to change text to audio
+    engine.say(Audio)
+    engine.runAndWait()
+
+text = input("Enter your text now: ") #input for text
+Speak(text) #calling the function which coverts the text to speech.
+
+
