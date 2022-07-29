408ad8958acba9db0931ec267d827fe00c2a9db0
SamAkim <63521202+SamAkim@users.noreply.github.com>
2022-07-29 14:52:39 +0530

    Add files via upload

---
+++ barcodegenerator'.py
@@ -0,0 +1,9 @@
+from barcode import EAN13
+from barcode.writer import ImageWriter
+num_of_barcodes=int(input("Enter the number of Barcodes you want to generate"))
+numbers=range(num_of_barcodes)
+for i in numbers:
+    id=input(" Give 12-Digit numbers for your barcode id: ")
+    my_code=EAN13(id,writer=ImageWriter)
+    name = input("Give name for the barcode: ")
+    my_code.save(name)
