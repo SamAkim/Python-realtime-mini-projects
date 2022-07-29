d74e55ad0d6f34c99195b5a0cba6a449162061ee
SamAkim <63521202+SamAkim@users.noreply.github.com>
2022-07-29 14:53:54 +0530

    Update barcodegenerator'.py

--- barcodegenerator'.py
+++ barcodegenerator'.py
@@ -1,5 +1,6 @@
+#inorder to use this program you have to first install the barcode pacakge in python.
 from barcode import EAN13
-from barcode.writer import ImageWriter
+from barcode.writer import ImageWriter 
 num_of_barcodes=int(input("Enter the number of Barcodes you want to generate"))
 numbers=range(num_of_barcodes)
 for i in numbers:
